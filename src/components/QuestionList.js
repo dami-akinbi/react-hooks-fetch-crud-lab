32e87350e90d900b35eb6f1f68d9048220883a27
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-21 14:45:09 +0100

    GET request successfully built

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/questions`)
+      .then((r) => r.json())
+      .then((data) => {
+        // console.log(data);
+        setQuestions(data);
+      });
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {
+          /* display QuestionItem components here after fetching */
+          questions.map((question) => (
+            <QuestionItem key={question.id} question={question} />
+          ))
+        }
+      </ul>
     </section>
   );
 }
